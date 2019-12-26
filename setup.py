dc521f8fb22f40451bdca4c78b783f8123cd34ab
braddyH <yingxu.he1998@gmail.com>
2019-12-26 23:04:44 +0800

    edit setup info

--- setup.py
+++ setup.py
@@ -3,13 +3,13 @@ from distutils.core import setup
 setup(
   name = 'genetic_algorithm',
   packages = ['genetic_algorithm'],
-  version = '0.1.3',
+  version = '0.2.1',
   license='MIT',
   description = 'A python package implementing the genetic algorithm',
   author = 'He Yingxu',
   author_email = 'yingxu.he1998@gmail.com',
   url = 'https://github.com/YingxuH/genetic_algorithm',
-  download_url = 'https://github.com/YingxuH/genetic_algorithm/archive/0.1.tar.gz',
+  download_url = 'https://github.com/YingxuH/genetic_algorithm/archive/0.2.1.tar.gz',
   keywords = ['genetic algorithm'],
   install_requires = [
     "numpy",
--- test.py
+++ test.py
@@ -5,8 +5,8 @@ from genetic_algorithm import GeneticAlgorithm
 #
 
 
-def func(a,b,c):
-    return -(a-1)**2 - (b-3)**2 - (c-5) ** 2
+def func(a,b,c,d):
+    return -(a-1)**2 - (b-3)**2 - (c-5) ** 2 - len(d)
 
 
 def fitness(params):
@@ -15,7 +15,8 @@ def fitness(params):
 
 param_space = {"a": {'type': 'float', 'range':[0, 2]},
                "b": {'type': 'float', 'range':[1, 5]},
-               "c": {'type': 'float', 'range':[3, 7]}}
+               "c": {'type': 'int', 'range':[3, 7]},
+               "d": {'type': 'object', 'range':['qwe', 'qw', 'a', '234r']}}
 #
 # ga = genetic_optimization(input_model=fitness, param_space=param_space, pop_size=100, num_parents=3,
 #                      max_num_generations=100, mutation_prob=0.2, stoping_rounds=5, integer_params=[])['best params']
